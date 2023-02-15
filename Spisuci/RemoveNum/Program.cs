7e3445f55405370a776ae48da9cfc81c194b9dce
VickyyB <viktoria.bozhdarova@gmail.com>
2023-02-15 09:35:30 +0200

    Ne  e dovursheno!

---
+++ Spisuci/RemoveNum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int last = nums.Last();
+            //int last1 = nums[nums.Count - 1];
+            nums.RemoveAll(x => x == last);
+            Console.WriteLine(string.Join(" ",nums));
+        }
+    }
+}
