d6f3d5cd4758f4dad5cbdca4dc2ae7cf6676106a
VickyyB <viktoria.bozhdarova@gmail.com>
2023-03-01 09:10:49 +0200

    Vs tochno

--- Spisuci/ListManipulator/Program.cs
+++ Spisuci/ListManipulator/Program.cs
@@ -13,24 +13,63 @@ namespace ListManipulator
             {
                 string[] input = Console.ReadLine().Split().ToArray();
                 string command = input[0];
-                    if (command=="print")
+                if (command == "print")
                 {
                     break;
                 }
 
-                    switch(command)
+                switch (command)
                 {
-                    case "add": var index = int.Parse(input[1]);
+                    case "add":
+                        var index = int.Parse(input[1]);
                         var element = int.Parse(input[2]);
                         nums.Insert(index, element);
-                            break;
-                    case "addMany":
+                        break;
 
+                    case "addMany":
+                        index = int.Parse(input[1]);
+                        for (int i = input.Count() - 1; i >= 2; i--)
+                        {
+                            nums.Insert(index, int.Parse(input[i]));
+                        }
                         break;
                     case "contains":
+                        element = int.Parse(input[1]);
+                        index = nums.IndexOf(element);
+                        Console.WriteLine(index);
                         break;
+                    case "remove":
+                        index = int.Parse(input[1]);
+                        nums.RemoveAt(index);
+                        break;
+                    case "shift":
+                        var pos = int.Parse(input[1]);
+                        //for(int i =0;pos%nums.Count;i++)
+                        for (int i = 0; i < pos; i++)
+                        {
+                            int elem = nums[0];
+                            nums.RemoveAt(0);
+                            nums.Add(elem);
+                        }
+                        break;
+                    case "sumPairs":
+                        List<int> result = new List<int>();
+                        for (int i = 0; i < nums.Count; i = i + 2)
+                        {
+                            result.Add( nums[i] + nums[i + 1]);
+                        }
+                        nums = result;
+                        Console.WriteLine(string.Join(" ", nums));
+                        break;
+                    default:
+                        break;
+
+
+
                 }
             }
+            Console.WriteLine(string.Join(", ", nums));
         }
     }
 }
+
